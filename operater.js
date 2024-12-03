e2da9561b8d60505dafab02dade191a3221c8fe9
VikashVerma62 <vikashverma626506@gmail.com>
2024-12-03 00:36:08 -0800

    update

---
+++ operater.js
@@ -0,0 +1,61 @@
+// console.log("hello world");  
+// comments = this is a single line comment
+
+// this is code that prints hello world that prints
+// hello world on our console
+
+
+
+
+// =============================operaters=====================================
+// Artimetic operaters
+
+// +,-,*,/
+// moodulus operater 
+// Exponentiation operater 
+// Increment 
+// Decrement            Increment and Decrement operater are called unary operaters
+
+
+
+// let a=5;
+// let b=5;
+// console.log("a=", a,"&b=" , b );
+// console.log("a+b=",a+b );
+// console.log("a-b=",a-b);
+// console.log("a*b=", a*b );
+// console.log("a/b=", a/b);
+// console.log("a/b=", a%b);
+// console.log("a/b=", a**b);   // double astrisic is used to find the power in this example we find a to the power b;
+
+
+
+// unary operater 
+// let a=5;
+// let b=5;
+// console.log("a=", a,"&b=" , b );
+// a++;
+// console.log(a);
+
+// // Assingment operater in js
+// // =  , +=   , -=  , *= , %= , **=
+
+// let a=5;
+// let b=5;
+// a+=4;
+// console.log(a);
+// a-=4;
+// console.log(a);
+// comperision operater 
+// equal to ==
+// !equal to !=
+// Equal to and type ===
+// Not equal to & type !==
+
+
+// =======================comperision operater===================
+let a=5;
+let b="3";
+console.log("5!=5)", a!=b);
+
+console.log(a===b); // === is srictrictly check the type of variable and then compare the value;.
