bcd55d22fcf63b3d3260a685e9b4adfd6fc83c55
VikashVerma62 <vikashverma626506@gmail.com>
2024-12-05 07:32:02 -0800

    update

---
+++ practise2.js
@@ -0,0 +1,16 @@
+function run(){
+    let num=parseInt(prompt("enter the number"));
+    if(num>0){
+        // alert("number is positive");
+        // prompt("number is positive");
+        confirm("number is positive")
+
+
+    }
+    else{
+        // alert("number is negative");
+        // prompt("number is negative");
+        confirm("number is negative")
+    }
+}
+run();
