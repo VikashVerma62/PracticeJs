4c728ecba8b1297ac97c21daed7700840a7032b6
VikashVerma62 <vikashverma626506@gmail.com>
2024-12-05 21:56:53 -0800

    update

--- practise2.js
+++ practise2.js
@@ -11,6 +11,30 @@ function run(){
         // alert("number is negative");
         // prompt("number is negative");
         confirm("number is negative")
+        
     }
 }
 run();
+
+let run1=()=>{
+    let age=prompt("enter your age")
+     let result=age>18? prompt("you are eligible for vote"): prompt("you are not eligible for vote");
+    alert(result);
+    
+
+
+}
+
+let run2=()=>{
+    let course1=confirm("course completed"); 
+    let course2=confirm("certificat completed");
+    if(course1 && course2){
+alert("you are certified");
+    }
+    else{
+        alert("you are not certified")
+    }
+}
+let run=()=>{
+    
+}
