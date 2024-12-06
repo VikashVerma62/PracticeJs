d8fe1b2dd16ebe672f3f7539a7e42daf42ffb37e
VikashVerma62 <vikashverma626506@gmail.com>
2024-12-06 01:10:51 -0800

    update

--- practise2.js
+++ practise2.js
@@ -1,40 +1,91 @@
-function run(){
-    let num=parseInt(prompt("enter the number"));
-    if(num>0){
-        // alert("number is positive");
-        // prompt("number is positive");
-        confirm("number is positive")
+// function run(){
+//     let num=parseInt(prompt("enter the number"));
+//     if(num>0){
+//         // alert("number is positive");
+//         // prompt("number is positive");
+//         confirm("number is positive")
 
 
-    }
-    else{
-        // alert("number is negative");
-        // prompt("number is negative");
-        confirm("number is negative")
+//     }
+//     else{
+//         // alert("number is negative");
+//         // prompt("number is negative");
+//         confirm("number is negative")
         
-    }
-}
-run();
+//     }
+// }
+//run();
 
-let run1=()=>{
-    let age=prompt("enter your age")
-     let result=age>18? prompt("you are eligible for vote"): prompt("you are not eligible for vote");
-    alert(result);
+// let run1=()=>{
+//     let age=prompt("enter your age")
+//      let result=age>18? prompt("you are eligible for vote"): prompt("you are not eligible for vote");
+//     alert(result);
     
 
 
-}
+// }
 
-let run2=()=>{
-    let course1=confirm("course completed"); 
-    let course2=confirm("certificat completed");
-    if(course1 && course2){
-alert("you are certified");
-    }
-    else{
-        alert("you are not certified")
-    }
-}
-let run=()=>{
-    
-}
+// let run2=()=>{
+//     let course1=confirm("course completed"); 
+//     let course2=confirm("certificat completed");
+//     if(course1 && course2){
+// alert("you are certified");
+//     }
+//     else{
+//         alert("you are not certified")
+//     }
+// }
+// let run3= ()=> {
+
+//     let price = prompt("enter price of item");
+
+
+//     let dis= confirm("you want discount");
+
+//     if ( dis){
+
+//         dis= (price-(price*0.10))
+
+//         alert(dis)
+//     }
+
+//     else {
+//         alert("no discount")
+
+//     }
+// }
+
+// let run4=()=>{
+//     let marks=prompt("enter marks");
+//     if(marks>=33){
+//         alert("pass");
+//     }
+//     else{
+//         alert("fail")
+//     }
+// }
+// let run5=()=>{
+//     confirm("are you sure");
+//     let yes= confirm(yes);
+//     if(yes){
+//         alert("deleted");
+//     }else{
+//         alert("cencelled");
+//     }
+// }
+// let run6 =() =>{
+    // let number=prompt("enter number");
+    // if(number>0){
+        // alert("positive number");
+    // } else{
+        // alert("negative number");
+    // }
+// }
+// let run7=()=>{
+//     let number=prompt("enter the number");
+//     if(number%2==0){
+//         alert(" number is even");
+//     } else{
+//         alert(" number is odd");
+//     }
+// }
