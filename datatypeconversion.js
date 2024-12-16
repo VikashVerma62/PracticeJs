56c8a7d455165d96093eadeb1fb0669130200d32
VikashVerma62 <vikashverma626506@gmail.com>
2024-12-16 06:32:57 -0800

    update

---
+++ datatypeconversion.js
@@ -0,0 +1,17 @@
+let score="33asf";  
+console.log(typeof score);
+console.log(typeof(score));
+let valueInNumber=Number(score);// to convert in number
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+//"33"=> 33
+// "33abc"=>NaN
+// true=>1
+let isLoggedIn=1;
+let booleanIsLoggenIn=Boolean(isLoggedIn)// first letter of the converter is always capital
+console.log(booleanIsLoggenIn);
+
+let someNumber=33;
+let stringNumber=String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
