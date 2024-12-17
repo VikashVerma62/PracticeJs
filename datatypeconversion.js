edf6112b7bc60f788531ab0043e90f6df660c894
VikashVerma62 <vikashverma626506@gmail.com>
2024-12-17 08:17:33 -0800

    update

--- datatypeconversion.js
+++ datatypeconversion.js
@@ -1,17 +1,56 @@
-let score="33asf";  
+ let score="33asf";  
 console.log(typeof score);
 console.log(typeof(score));
-let valueInNumber=Number(score);// to convert in number
+let valueInNumber=Number(score);          // to convert in number
 console.log(typeof valueInNumber);
 console.log(valueInNumber);
+
+
+
 //"33"=> 33
 // "33abc"=>NaN
 // true=>1
+
+
+
 let isLoggedIn=1;
-let booleanIsLoggenIn=Boolean(isLoggedIn)// first letter of the converter is always capital
+let booleanIsLoggenIn=Boolean(isLoggedIn)           // first letter of the converter is always capital
 console.log(booleanIsLoggenIn);
+
+
+
+
+
+
+
+
 
 let someNumber=33;
 let stringNumber=String(someNumber);
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+// *********************operations*********************
+// let value=3
+// let negvalue=-value;
+// console.log(negvalue);
+
+
+let str1="hello";
+let str2=" hitesh";
+ let str3=str1+str2;
+ console.log(str3)
+
+ //  if there is string in first then it convert it in a string and if other one than the first one is done and merge the string in last.
+console.log("1"+2);
+console.log(1+"2");
+console.log(1+2+"2");
+console.log( + true);
+
+let num1,num2, num3;
+num1=num2=num3=2+2;
+console.log(num1,num2, num3)
+
+let gameCounter=100;
+++gameCounter;
+console.log(gameCounter);
