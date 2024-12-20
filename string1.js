1233b1ecbcc3ccf3f03b5d669667ebc656203c0c
VikashVerma62 <vikashverma626506@gmail.com>
2024-12-20 05:21:46 -0800

    update

---
+++ string1.js
@@ -0,0 +1,10 @@
+const name="hitesh";
+const repoCount=50;
+console.log(name + repoCount + " value");   //constatination of string;
+
+console.log(`Hello my is ${name} and my repo count is ${repoCount}`);
+
+
+const gameName=new String ('hiteshhc');
+console.log(gameName.length);
+console.log(gameName[0]);
